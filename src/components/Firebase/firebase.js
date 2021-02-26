667347b10b0a2513efc32f5a44d712f4df7ae1fb
Melinda <melinda.sava.td@gmail.com>
2021-02-26 13:14:44 +0100

    Chat app: defining API, Fetch List, creating item in list

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -62,6 +62,9 @@ class Firebase {
     // *** User API ***
     user = uid => this.db.ref(`users/${uid}`);
     users = () => this.db.ref('users');
+    // *** Message API ***
+    message = uid => this.db.ref(`messages/${uid}`);
+    messages = () => this.db.ref('messages');
 }
 
 export default Firebase;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { compose } from 'recompose';
 import { withAuthorization, withEmailVerification } from '../Session';
-
+import { withFirebase } from '../Firebase';
 
 const HomePage = () => (
     <div>
         <h1>Home Page</h1>
         <p>The Home Page is accessible by every signed in user.</p>
+
+        <Messages />
+
     </div>
+);
+
+class MessagesBase extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: '',
+            loading: false,
+            messages: [],
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ loading: true });
+
+        this.props.firebase.messages().on('value', snapshot => {
+
+            const messageObject = snapshot.val();
+
+            if (messageObject) {
+
+                const messageList = Object.keys(messageObject).map(key => ({
+                    ...messageObject[key],
+                    uid: key,
+                }));
+
+                this.setState({
+                    messages: messageList,
+                    loading: false,
+                });
+            } else {
+                this.setState({ messages: null, loading: false });
+            }
+        });
+    }
+    componentWillUnmount() {
+        this.props.firebase.messages().off();
+    }
+    render() {
+        const { text, messages, loading } = this.state;
+
+        return (
+            <div>
+                {loading && <div>Loading ...</div>}
+
+                {messages ? (
+                    <MessageList messages={messages} />
+                ) : (
+                        <div>There are no messages ...</div>
+                    )}
+
+                <form onSubmit={this.onCreateMessage}>
+                    <input
+                        type="text"
+                        value={text}
+                        onChange={this.onChangeText}
+                    />
+                    <button type="submit">Send</button>
+                </form>
+
+            </div>
+        );
+
+    }
+}
+
+const MessageList = ({ messages }) => (
+    <ul>
+        {messages.map(message => (
+            <MessageItem key={message.uid} message={message} />
+        ))}
+    </ul>
+);
+
+const MessageItem = ({ message }) => (
+    <li>
+        <strong>{message.userId}</strong> {message.text}
+    </li>
 );
 
 const condition = authUser => !!authUser;
 
-
+const Messages = withFirebase(MessagesBase);
 export default compose(
     withEmailVerification,
     withAuthorization(condition),
