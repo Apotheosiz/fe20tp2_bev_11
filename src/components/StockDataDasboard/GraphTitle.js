a8af6587056ba8c0f2c5a3e863ed9dbfa799a1dd
Melinda <melinda.sava.td@gmail.com>
2021-03-18 12:39:20 +0100

    edited last price to only show two decimals

--- src/components/StockDataDasboard/GraphTitle.js
+++ src/components/StockDataDasboard/GraphTitle.js
@@ -9,14 +9,15 @@ const GraphTitle = ({ comp, data }) => {
       return  (Math.round(num * 100) / 100).toFixed(2);
     }
 
+    const lastPrice = twoDecim(data[data.length - 1].price);
     const percDiff = twoDecim((current-former)/current*100.0);
     const diff = twoDecim(current-former);
 return (<>
 {/* {console.log(prev)} */}
     <h2><span>{comp.symbol}</span>: {comp.name}</h2>
     <h1>
-        {data[data.length - 1].price}{comp.currency} 
-        
+        {lastPrice}{comp.currency} 
+
         {percDiff >=0 ? 
             <span style={{ color: '#137333'}}>↑{percDiff}%</span>
             : <span style={{ color: '#a50e0e' }}>↓{percDiff}%</span>} 
