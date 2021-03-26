90f3d281a414465b3963463c35374d5b6c5fcb76
Vaanessa <vanessa.martinez@student.kyh.se>
2021-03-26 14:57:49 +0100

    styled ticker search box

--- src/components/StockDataDasboard/index.js
+++ src/components/StockDataDasboard/index.js
@@ -2,6 +2,50 @@ import { useState, useEffect } from 'react';
 import CompanyData from './CompanyData.js';
 import { withFirebase } from '../Firebase';
 import PreviewPanel from './PreviewPanel';
+import styled from 'styled-components';
+
+const FormWrapper = styled.div`
+    width: 100%;
+    form {
+        width: 95%;
+        max-width: 900px;
+        margin: 20px auto;
+        border-bottom: 1px solid black;
+        display: flex;
+        justify-content: space-between;
+
+        input {
+            width: 90%;
+            border: none;
+            outline: none;
+            
+        }
+
+        button {
+            border: none;
+            background: transparent;
+            padding: 10px;
+            cursor: pointer;
+            background: #d3dbde;
+            border-radius: 50%;
+            width: 35px;
+            height: 35px;
+            margin: 5px;
+            
+            svg {
+                fill: #000;
+           
+            }
+        }
+    }
+   
+    form:hover,
+    form:active,
+    form:focus{
+        box-shadow: 5px 10px #888888;
+    }
+    
+`
 
 const StockDataDashboard = ({ authUser, firebase, comp, setComp }) => {
     console.log(authUser.ticker);
@@ -51,19 +95,25 @@ const StockDataDashboard = ({ authUser, firebase, comp, setComp }) => {
 
     return (
         <div>
-            
+            <FormWrapper>            
+                <form onSubmit={onSubmit}>
+                    <input
+                        name="searchCompany"
+                        onChange={onChange}
+                        type="text"
+                        placeholder="Company name or ticker"
+                        value={searchTerm}
+                    />
+                    <button type="submit" >
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="svg-inline--fa fa-search fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <title>Search</title>
+                            <path fill="#000" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg>
+                    </button>
+                </form>
+            </FormWrapper>
+
             {user && <PreviewPanel user={user} setCompanyTicker={setCompanyTicker} setComp={setComp} delTicker={delTicker} />}
 
-            <form onSubmit={onSubmit}>
-                <input
-                    name="searchCompany"
-                    onChange={onChange}
-                    type="text"
-                    placeholder="Company name or ticker"
-                    value={searchTerm}
-                />
-                <input type="submit" value="Search" />
-            </form>
             {results ?
                 (results.length > 0) ?
                     <div>
