a4eece4916d995e3cdc4857cfb5f68482767a816
cr4y0n <filipaspajic@gmail.com>
2021-04-08 14:51:25 +0200

    put preview panels over search field on desktop

--- src/components/StockDataDasboard/PreviewPanel.js
+++ src/components/StockDataDasboard/PreviewPanel.js
@@ -7,12 +7,15 @@ margin: 30px auto;
 display:flex;
 flex-wrap:wrap;
 justify-content:center;
+@media screen and (min-width:550px) {
+    margin: 15px auto;
+}
 `;
 
 const PreviewPanel = ({ user, setCompanyTicker, setComp, delTicker }) => {
 
     return (
-        <section>
+        <section className="preview-panel">
             <Panel>                
                 {user && user.tickers && Object.keys(user.tickers).map(ticker => <PreviewItem user={user} comp={user.tickers[ticker]} ticker={ticker} setCompanyTicker={setCompanyTicker} setComp={setComp} delTicker={delTicker} key={ticker} />)}
             </Panel>
