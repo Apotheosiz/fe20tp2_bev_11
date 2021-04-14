272e7635b31ce46b242efa5049ca07fe0ba1270f
Melinda <melinda.sava.td@gmail.com>
2021-04-14 09:00:55 +0200

    finished styling messages, still need aligning

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -11,8 +11,7 @@ import pic6 from '../../img/profiles/6.png';
 import pic7 from '../../img/profiles/7.png';
 import pic8 from '../../img/profiles/8.png';
 import styled from 'styled-components';
-import { DeletePic } from '../svgImg/WelcomePic';
-import { EditPic } from '../svgImg/WelcomePic';
+import { DeletePic, EditPic, SavePic, UndoPic } from '../svgImg/WelcomePic';
 
 const picArr = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8];
 
@@ -26,236 +25,266 @@ margin:0 auto;
 `
 
 const StyledLi = styled.li`
+display: flex;
+justify-content: space-between;
+padding: 20px;
+background: #F8C3C3;
+border-radius: 20px;
+border-bottom: 1px solid #efefef;
+textarea{
+  border: none;
+  border-bottom: 2px solid #44062B;
+  background:transparent;
+  padding:2px;
+  font-size:inherit;
+  font-family:inherit;
+}
+.profile-and-text{
     display: flex;
-    justify-content: space-between;
-    padding: 20px;
-    background: #F8C3C3;
-    border-radius: 20px;
-    border-bottom: 1px solid #efefef;
-    .profile-and-text{
-        display: flex;
-        align-items:flex-start;
+    align-items:flex-start;
+    flex: 1;
+    textarea{
+      align-self:flex-end;
+      flex-grow: 1;
     }
-    img{
-        //margin-bottom:10px;
-        &:hover{
-            top:0;
-            cursor: initial;
-        }
+}
+img{
+    &:hover{
+        top:0;
+        cursor: initial;
     }
-    form,
-    span{
-        display:flex;
-        align-items: center;
+}
+form,
+span{
+    display:flex;
+    align-items: center;
+}
+.buttons-wrapper{
+  align-self: flex-end;
+}
+form{
+    width:100%;
+    display:flex;
+    align-items:flex-end;
+    justify-content:space-between;
+    textarea{
+      flex:1;
     }
-    form{
-        width:100%;
-    }
+}
 `
 
 const ChatButton = styled.button`
 content:" ";
-    svg {
-        fill: #efefef;
-        &:hover {
-            fill: #44062B;
-        }
-    }
-
+width:${props => props.width ? props.width : "26px"};;
+height: 24px;
+padding:5px;
+border: none;
+background: transparent;
+display:flex;
+align-items:flex-end;
+cursor:pointer;
+&:hover{
+  position:relative;
+  top:-1px;
+}
 `
 
 class MessagesBase extends Component {
   constructor(props) {
-      super(props);
+    super(props);
 
-      this.state = {
-          text: '',
-          loading: false,
-          messages: [],
-      };
+    this.state = {
+      text: '',
+      loading: false,
+      messages: [],
+    };
   }
 
   onChangeText = event => {
-      this.setState({ text: event.target.value });
+    this.setState({ text: event.target.value });
   };
 
   onCreateMessage = (event, authUser) => {
 
-      this.props.firebase.messages().push({
-          text: this.state.text,
-          userId: authUser.uid,
-          username: authUser.username,
-          createdAt: this.props.firebase.serverValue.TIMESTAMP,
-          profilePic: authUser.profilePic,
-      });
+    this.props.firebase.messages().push({
+      text: this.state.text,
+      userId: authUser.uid,
+      username: authUser.username,
+      createdAt: this.props.firebase.serverValue.TIMESTAMP,
+      profilePic: authUser.profilePic,
+    });
 
-      this.setState({ text: '' });
-      event.preventDefault();
+    this.setState({ text: '' });
+    event.preventDefault();
   };
 
   onRemoveMessage = uid => {
-      this.props.firebase.message(uid).remove();
+    this.props.firebase.message(uid).remove();
   };
 
   onEditMessage = (message, text) => {
-      const { uid, ...messageSnapshot } = message;
+    const { uid, ...messageSnapshot } = message;
 
-      this.props.firebase.message(message.uid).set({
-          ...messageSnapshot,
-          text,
-          editedAt: this.props.firebase.serverValue.TIMESTAMP,
-      });
+    this.props.firebase.message(message.uid).set({
+      ...messageSnapshot,
+      text,
+      editedAt: this.props.firebase.serverValue.TIMESTAMP,
+    });
   };
 
   componentDidMount() {
-      this.setState({ loading: true });
+    this.setState({ loading: true });
 
-      this.props.firebase.messages().on('value', snapshot => {
+    this.props.firebase.messages().on('value', snapshot => {
 
-          const messageObject = snapshot.val();
+      const messageObject = snapshot.val();
 
-          if (messageObject) {
+      if (messageObject) {
 
-              const messageList = Object.keys(messageObject).map(key => ({
-                  ...messageObject[key],
-                  uid: key,
-              }));
+        const messageList = Object.keys(messageObject).map(key => ({
+          ...messageObject[key],
+          uid: key,
+        }));
 
-              this.setState({
-                  messages: messageList,
-                  loading: false,
-              });
-          } else {
-              this.setState({ messages: null, loading: false });
-          }
-      });
+        this.setState({
+          messages: messageList,
+          loading: false,
+        });
+      } else {
+        this.setState({ messages: null, loading: false });
+      }
+    });
   }
   componentWillUnmount() {
-      this.props.firebase.messages().off();
+    this.props.firebase.messages().off();
   }
   render() {
-      const { text, messages, loading } = this.state;
+    const { text, messages, loading } = this.state;
 
-      return (
-          <AuthUserContext.Consumer>
-                {authUser => (
-                    <StyledDiv>
-                      {loading && <div>Loading ...</div>}
+    return (
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <StyledDiv>
+            {loading && <div>Loading ...</div>}
 
-                      {messages ? (
-                          <MessageList
-                              authUser={authUser}
-                              messages={messages}
-                              onEditMessage={this.onEditMessage}
-                              onRemoveMessage={this.onRemoveMessage}
-                          />
-                      ) : (
-                          null
-                      )}
-                        <StyledLi>
-                        <StyledImg src={picArr[authUser.profilePic- 1]} alt="profile"/>
-                        <form onSubmit={event => this.onCreateMessage(event, authUser)}>
-                            <input
-                                type="text"
-                                value={text}
-                                onChange={this.onChangeText}
-                            />
-                            <ChatButton type="submit">Send</ChatButton>
-                        </form>
-                        </StyledLi>
-                    </StyledDiv>
-                )}
-          </AuthUserContext.Consumer>
-      );
+            {messages ? (
+              <MessageList
+                authUser={authUser}
+                messages={messages}
+                onEditMessage={this.onEditMessage}
+                onRemoveMessage={this.onRemoveMessage}
+              />
+            ) : (
+              null
+            )}
+            <StyledLi>
+              <StyledImg src={picArr[authUser.profilePic - 1]} alt="profile" />
+              <form onSubmit={event => this.onCreateMessage(event, authUser)}>
+                <textarea
+                  type="text"
+                  value={text}
+                  onChange={this.onChangeText}
+                />
+                <ChatButton type="submit">Send</ChatButton>
+              </form>
+            </StyledLi>
+          </StyledDiv>
+        )}
+      </AuthUserContext.Consumer>
+    );
 
   }
 }
 
 const MessageList = ({ authUser, messages, onEditMessage, onRemoveMessage }) => (
   <>
-      {messages.map(message => (
-          <MessageItem
-              authUser={authUser}
-              key={message.uid}
-              message={message}
-              onEditMessage={onEditMessage}
-              onRemoveMessage={onRemoveMessage}
-          />
-      ))}
+    {messages.map(message => (
+      <MessageItem
+        authUser={authUser}
+        key={message.uid}
+        message={message}
+        onEditMessage={onEditMessage}
+        onRemoveMessage={onRemoveMessage}
+      />
+    ))}
   </>
 );
 
 class MessageItem extends Component {
   constructor(props) {
-      super(props);
+    super(props);
 
-      this.state = {
-          editMode: false,
-          editText: this.props.message.text,
-      };
+    this.state = {
+      editMode: false,
+      editText: this.props.message.text,
+    };
   }
 
   onToggleEditMode = () => {
-      this.setState(state => ({
-          editMode: !state.editMode,
-          editText: this.props.message.text,
-      }));
+    this.setState(state => ({
+      editMode: !state.editMode,
+      editText: this.props.message.text,
+    }));
   };
 
   onChangeEditText = event => {
-      this.setState({ editText: event.target.value });
+    this.setState({ editText: event.target.value });
   };
 
   onSaveEditText = () => {
-      this.props.onEditMessage(this.props.message, this.state.editText);
+    this.props.onEditMessage(this.props.message, this.state.editText);
 
-      this.setState({ editMode: false });
+    this.setState({ editMode: false });
   };
 
   render() {
 
-      const { authUser, message, onRemoveMessage } = this.props;
-      const { editMode, editText } = this.state;
+    const { authUser, message, onRemoveMessage } = this.props;
+    const { editMode, editText } = this.state;
 
-      return (
-          <StyledLi>
-                <span className="profile-and-text">
-                    <StyledImg src={picArr[message.profilePic - 1]} alt="profile"/>
-                    {editMode ? (
-                        <input
-                            type="text"
-                            value={editText}
-                            onChange={this.onChangeEditText}
-                        />
-                    ) : (
-                        <div>
-                            <strong>{message.username}</strong> 
-                            <p>{message.text}</p>
-                        </div>
-                    )}
-                </span>
-              {authUser.uid === message.userId && (
-                  <span>
-                      {editMode ? (
-                            <span>
-                              <ChatButton onClick={this.onSaveEditText}>Save</ChatButton>
-                              <ChatButton onClick={this.onToggleEditMode}>Reset</ChatButton>
-                            </span>
-                      ) : (
-                            <ChatButton onClick={this.onToggleEditMode}>
-                              Edit{/*<EditPic />*/}
-                            </ChatButton>
-                      )}
+    return (
+      <StyledLi>
+        <span className="profile-and-text">
+          <StyledImg src={picArr[message.profilePic - 1]} alt="profile" />
+          {editMode ? (
+            <textarea
+              type="text"
+              value={editText}
+              onChange={this.onChangeEditText}
+            />
+          ) : (
+            <div>
+              <strong>{message.username}</strong>
+              <p>{message.text}</p>
+            </div>
+          )}
+        </span>
+        {authUser.uid === message.userId && (
+          <span className="buttons-wrapper">
+            {editMode ? (
+              <span>
+                <ChatButton title="Save message" onClick={this.onSaveEditText}>
+                  <SavePic width="100%" margin="0 auto" />
+                </ChatButton>
+                <ChatButton title="Restore message" onClick={this.onToggleEditMode}>
+                  <UndoPic width="100%" margin="0 auto" />
+                </ChatButton>
+              </span>
+            ) : (
+              <ChatButton onClick={this.onToggleEditMode}>
+                <EditPic width="100%" margin="0 auto" />
+              </ChatButton>
+            )}
 
-                      {!editMode && (    
-                            <ChatButton onClick={() => onRemoveMessage(message.uid)}>                    
-                              Delete{/*<DeletePic />*/}
-                            </ChatButton>
-                      )}
-                  </span>
-              )}
-          </StyledLi>
-      );
+            {!editMode && (
+              <ChatButton onClick={() => onRemoveMessage(message.uid)}>
+                <DeletePic width="70%" margin="0 auto" />
+              </ChatButton>
+            )}
+          </span>
+        )}
+      </StyledLi>
+    );
   }
 }
 const Messages = withFirebase(MessagesBase);
